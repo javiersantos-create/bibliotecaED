ded4e0648f5e3901ad458902b8fbfc931c7ef3c7
Javier <javier.santos@insbaixcamp.cat>
2026-05-06 10:37:02 +0200

    He añadido cosas en la main porque he añadido categoria en Libro y he tenido que meter categoria para instanciar

--- main.java
+++ main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
 
-        Llibre llibre1 = new Llibre("1984", "George Orwell");
-        Llibre llibre2 = new Llibre("El petit príncep", "Antoine de Saint-Exupéry");
+        Llibre llibre1 = new Llibre("1984","Novel·la", "George Orwell");
+        Llibre llibre2 = new Llibre("El petit príncep","Històrica", "Antoine de Saint-Exupéry");
 
         biblioteca.afegirLlibre(llibre1);
         biblioteca.afegirLlibre(llibre2);
